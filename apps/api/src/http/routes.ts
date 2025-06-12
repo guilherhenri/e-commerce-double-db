29426a84f2344b14bd47f263d0fbabba99249b9b
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-12 14:39:11 -0300

    feat(routing): add update user route
    - Added a new `PUT /users/:id` route to `src/http/routes.ts`.
    - This route is mapped to the `updateUser` controller.
    - Expands the API with an endpoint for modifying existing user information.

--- apps/api/src/http/routes.ts
+++ apps/api/src/http/routes.ts
@@ -2,8 +2,10 @@ import type { FastifyInstance } from 'fastify'
 
 import { profile } from './controllers/users/profile'
 import { registerUser } from './controllers/users/register'
+import { updateUser } from './controllers/users/update'
 
 export async function routes(app: FastifyInstance) {
   app.get('/users/:id/profile', profile)
   app.post('/users', registerUser)
+  app.put('/users/:id', updateUser)
 }
