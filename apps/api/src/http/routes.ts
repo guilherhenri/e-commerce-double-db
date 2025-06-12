f1244585992a1643729c8af370730a18fc4a09fa
Guilherme Henrique <guilherhenri12@gmail.com>
2025-06-12 14:30:13 -0300

    feat(routing): add get user profile route
    - Added a new `GET /users/:id/profile` route to `src/http/routes.ts`.
    - This route is mapped to the `profile` controller.
    - Expands the API with an endpoint for retrieving specific user details.

--- apps/api/src/http/routes.ts
+++ apps/api/src/http/routes.ts
@@ -1,7 +1,9 @@
 import type { FastifyInstance } from 'fastify'
 
+import { profile } from './controllers/users/profile'
 import { registerUser } from './controllers/users/register'
 
 export async function routes(app: FastifyInstance) {
+  app.get('/users/:id/profile', profile)
   app.post('/users', registerUser)
 }
